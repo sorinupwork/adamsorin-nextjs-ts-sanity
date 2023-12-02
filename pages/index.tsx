194f5db217b51b48e69e2251890d7bfa93eff43f
sorinupwork <sorin.upwork@gmail.com>
2023-12-03 00:46:04 +0200

    add projects page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Hero from '@/components/Hero';
 import About from '@/components/About';
 import WorkExperience from '@/components/WorkExperience';
 import Skills from '@/components/Skills';
+import Projects from '@/components/Projects';
 
 export default function Home() {
   return (
@@ -27,12 +28,13 @@ export default function Home() {
         <WorkExperience />
       </section>
 
-      {/* Skills */}
       <section id="skills" className="snap-start">
         <Skills />
       </section>
 
-      {/* Projects */}
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
 
       {/* Contact Me */}
     </div>
